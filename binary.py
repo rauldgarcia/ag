d350678a6fc198e42f45ab1284e11b00b20f2e71
Raul Garcia <rauld.garcia95@gmail.com>
2023-04-15 20:59:51 -0600

    Se agrega cruza

--- binary.py
+++ binary.py
@@ -9,6 +9,7 @@ sup=10
 #sup=5.12
 precision=3
 npoblacion=4
+pcruza=0.5
 
 l=int(math.log2(((sup-inf)*(10**precision))+0.9))
 
@@ -60,7 +61,7 @@ def seleccionpadres(pob):
     padres=[] #crea matriz de largo de la poblacion
     ptr=random.random()
     sum=0
-    for i in range(npoblacion-1):
+    for i in range(npoblacion):
         sum+=pob[i][3]
         f=True
         while f!=False:
@@ -71,8 +72,37 @@ def seleccionpadres(pob):
 
     return(padres)
 
+def cruza(pad):
+    shijos=[[k for k in range(4)]for l in range(npoblacion)] #crea una matriz de largo de los hijos
+    for par in range(0,len(pad)-1,2):
+        p1=pad[par][0]
+        p2=pad[par+1][0]
+
+        p=flip(pcruza) #volado si se cruzan o no
+        if p==1:
+            h1=np.zeros((l*d))
+            h2=np.zeros((l*d))
+
+            for ele in range(len(p1)):
+                cr=flip(0.5)
+                if cr==1:
+                    h1[ele]=p2[ele]
+                    h2[ele]=p1[ele]
+
+                else:
+                    h1[ele]=p1[ele]
+                    h2[ele]=p2[ele]
+
+            shijos[par][0]=h1
+            shijos[par+1][0]=h2
+
+        else:
+            shijos[par][0]=p1
+            shijos[par+1][0]=p2
+
+    return shijos
+
 poblacion=[[i for i in range(4)]for j in range(npoblacion)] #crea matriz de largo de la poblacion
-print(poblacion)
 for individuo in range(npoblacion):
     poblacion[individuo][0]=crea()
     poblacion[individuo][1]=evalua(poblacion[individuo][0])
@@ -81,3 +111,6 @@ print(poblacion)
 
 padres=seleccionpadres(poblacion)
 print(padres)
+
+hijos=cruza(padres)
+print(hijos)
